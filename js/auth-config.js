643d02fb6fb2bc7829dd6a6dac599174a9249bbf
David Andre Rivera Celis <david.rvrcls@gmail.com>
2026-08-19 15:42:51 -0600

    Configurar dominio de correo institucional (@uanl.edu.mx)

--- js/auth-config.js
+++ js/auth-config.js
@@ -1,3 +1,2 @@
 // Dominio de correo institucional permitido para el registro de maestros.
-// Cámbialo por el dominio real de la escuela (ej. "@etao1.edu.mx") y súbelo.
-export const DOMINIO_INSTITUCIONAL = "@tu-escuela.edu.mx";
+export const DOMINIO_INSTITUCIONAL = "@uanl.edu.mx";
