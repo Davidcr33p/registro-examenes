c7cc9d7b2c6b90d419171050354d9cb9bdd0d4c1
David Andre Rivera Celis <david.rvrcls@gmail.com>
2026-08-19 15:18:52 -0600

    Configurar credenciales reales del proyecto de Firebase

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,10 +4,10 @@
 // Ver README.md para la guía paso a paso.
 
 export const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "tu-proyecto.firebaseapp.com",
-  projectId: "tu-proyecto",
-  storageBucket: "tu-proyecto.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAFGjmplxb7Gv5ZIGKJSta4u9-or1S5J-A",
+  authDomain: "registro-examenes-eiao.firebaseapp.com",
+  projectId: "registro-examenes-eiao",
+  storageBucket: "registro-examenes-eiao.firebasestorage.app",
+  messagingSenderId: "1021380613521",
+  appId: "1:1021380613521:web:68f6244834c613ae56f630"
 };
